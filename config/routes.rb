56cf8a637f27e4b0e1d7c84046f8617da61f843e
petenut <min.sb.kim@gmail.com>
2016-06-27 11:59:05 -0700

    mades signups/controllers for sessions and users

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-
+  root to: "users#index"
   #user routes
   resources :users
+  get "sign_in", to: "sessions#new"
+  #session routes
+  post "/sessions", to: "sessions#create"
   #art routes
   resources :arts
   #tags routes
