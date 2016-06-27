dd31326dc88d95af8a0e150980be14d7b19231c1
cfcrawford <camilafcrawford@gmail.com>
2016-06-27 10:54:45 -0700

    adds models

---
+++ db/migrate/20160627175101_create_arts.rb
@@ -0,0 +1,9 @@
+class CreateArts < ActiveRecord::Migration
+  def change
+    create_table :arts do |t|
+      t.integer :user_id
+
+      t.timestamps null: false
+    end
+  end
+end
---
+++ db/migrate/20160627175315_create_tags.rb
@@ -0,0 +1,11 @@
+class CreateTags < ActiveRecord::Migration
+  def change
+    create_table :tags do |t|
+      t.integer :user_id
+      t.integer :art_id
+      t.string :category
+
+      t.timestamps null: false
+    end
+  end
+end
