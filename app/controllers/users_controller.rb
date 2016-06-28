97959e26f8dee1ccc5331800c0e1f0ddfcd03be1
LadyPi <ms.vanessa.farias.dm@gmail.com>
2016-06-28 09:32:09 -0700

    uploadcare: show and edit page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -58,7 +58,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :location, :password)
+    params.require(:user).permit(:first_name, :last_name, :email, :location, :password, :image)
   end
 
 end
