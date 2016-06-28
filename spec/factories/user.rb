34ce09b1d44975eb138b521bc53f333e61f813f8
petenut <min.sb.kim@gmail.com>
2016-06-27 17:26:56 -0700

    changes made to tests

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,7 +1,8 @@
 FactoryGirl.define do
   factory :user do
-    first_name    "John"
-    last_name     "Doe"
-    password "12345"
+    first_name    { Faker::Name.first_name }
+    last_name     { Faker::Name.last_name}
+    password      { Faker::Internet.password}
+    email         { Faker::Internet.email}
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,9 +4,18 @@ RSpec.describe User, type: :model do
 
   subject(:user) { FactoryGirl.create(:user)}
 
-  it "has first_name John" do
-    expect(user.first_name).to eq("John")
+  it "has first name" do
+    expect(user.first_name).not_to be_empty
   end
 
-end
+  it "has a last name" do
+    expect(user.last_name).to eq(user.last_name)
+  end
 
+  it "has an email and it is unique" do
+    expect(user.email).to_be_unique
+    expect(user.email).not_to be_empty
+  end
+
+
+end
