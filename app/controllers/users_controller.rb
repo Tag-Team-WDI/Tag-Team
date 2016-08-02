906426a41c7cd4f8603037ab936b99126f124b79
petenut <min.sb.kim@gmail.com>
2016-08-01 20:39:13 -0700

    adds logic for users_controller.rb for show method and changes error message color to red

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
-
+  # To see the index page and must be signed in
   before_action :require_login, only: :index
-
   def index
     @users = User.all
     render :index
@@ -9,31 +8,33 @@ class UsersController < ApplicationController
 
   def new
     @user = User.new
-
     render :new
   end
 
-
   def create
     @user = User.new(user_params)
-      if @user.save
-        session[:user_id] = @user.id
-        flash[:notice] = "Hello, #{@user.first_name}! Welcome to Tag!"
-        login(@user)
-        redirect_to "/users/#{@user.id}"
-      else
-        flash[:error] = @user.errors.full_messages.join(", ")
-        redirect_to new_user_path
-      end
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:notice] = "Hello, #{@user.first_name}! Welcome to Tag!"
+      login(@user)
+      redirect_to "/users/#{@user.id}"
+    else
+      flash[:error] = @user.errors.full_messages.join(", ")
+      redirect_to new_user_path
+    end
   end
-
-
+  
+  #user must be signed in to see their index otherwise will show the main page
   def show
-    @user = User.find(params[:id])
-    @user = current_user
-    render :show
+    if current_user
+      @user = User.find(params[:id])
+      @user = current_user
+      render :show
+    else
+      flash[:error] = "Must be signed in to see your profile!!"
+      redirect_to "/"
+    end
   end
-
 
   def edit
     @user = User.find(params[:id])
@@ -49,13 +50,11 @@ class UsersController < ApplicationController
     else
       flash[:error] = @user.errors.full_messages.join(", ")
       redirect_to "/"
+    end
   end
-end
 
   private
-
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email, :location, :password, :password_confirmation, :image)
   end
-
 end
